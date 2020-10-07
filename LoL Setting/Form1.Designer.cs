3e598223b6fe39a719fdce48eb8fb8b3d26b08a6
Peanut <zakariaspam0@gmail.com>
2020-10-07 13:03:56 +0100

    First Commit

---
+++ LoL Setting/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace LoL_Setting
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panelLeft = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.labeltime = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panelLeft.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelLeft
+            // 
+            this.panelLeft.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(35)))), ((int)(((byte)(38)))));
+            this.panelLeft.Controls.Add(this.label1);
+            this.panelLeft.Controls.Add(this.pictureBox1);
+            this.panelLeft.Location = new System.Drawing.Point(-3, -3);
+            this.panelLeft.Name = "panelLeft";
+            this.panelLeft.Size = new System.Drawing.Size(321, 543);
+            this.panelLeft.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(31, 340);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(265, 44);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "You Can Export and Import \r\nYou\'re Settings Free Here";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(15, 15);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(281, 312);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.PictureBox1_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(35)))), ((int)(((byte)(38)))));
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(323, 300);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(249, 34);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Export";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(35)))), ((int)(((byte)(38)))));
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.White;
+            this.button4.Location = new System.Drawing.Point(323, 229);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(249, 34);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Import";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(26)))), ((int)(((byte)(74)))));
+            this.label7.Location = new System.Drawing.Point(367, 164);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(10, 42);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "\r\n\r\n";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(26)))), ((int)(((byte)(74)))));
+            this.label8.Location = new System.Drawing.Point(557, -3);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(25, 30);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "x";
+            this.label8.Click += new System.EventHandler(this.Label8_Click);
+            // 
+            // button5
+            // 
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Image = ((System.Drawing.Image)(resources.GetObject("button5.Image")));
+            this.button5.Location = new System.Drawing.Point(551, 369);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(32, 32);
+            this.button5.TabIndex = 47;
+            this.button5.Text = " ";
+            this.button5.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Button5_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // labeltime
+            // 
+            this.labeltime.AutoSize = true;
+            this.labeltime.BackColor = System.Drawing.Color.Transparent;
+            this.labeltime.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labeltime.ForeColor = System.Drawing.Color.Black;
+            this.labeltime.Location = new System.Drawing.Point(324, 369);
+            this.labeltime.Name = "labeltime";
+            this.labeltime.Size = new System.Drawing.Size(42, 16);
+            this.labeltime.TabIndex = 48;
+            this.labeltime.Text = "label1";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(350, 2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(184, 126);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 49;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 402);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.labeltime);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.panelLeft);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panelLeft.ResumeLayout(false);
+            this.panelLeft.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panelLeft;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label labeltime;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ LoL Setting/Form1.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoL_Setting
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+           
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label8_Click(object sender, EventArgs e)
+        {
+
+            if (MessageBox.Show("Do You Really Want to Exite??", "Message", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                Application.Exit();
+
+            }
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://throwbin.io/U8KfVLB");
+
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            string initialPath = "";
+            bool allow = false;
+
+
+
+            if (Directory.Exists("C:/Riot Games"))
+            {
+                initialPath = "C:/Riot Games";
+                
+                allow = true;
+            }
+            else if (Directory.Exists("E:/Riot Games"))
+            {
+                
+                initialPath = "E:/Riot Games";
+                allow = true;
+            }
+            else if (Directory.Exists("D:/Riot Games"))
+            {
+                
+                initialPath = "D:/Riot Games";
+                allow = true;
+            }
+            else
+            {
+                MessageBox.Show("wa zpi tadir l file f c ola e ola d");
+            }
+
+
+
+
+
+
+            if (allow)
+            {
+
+
+                //<game.cfg>
+                string gameCfg;
+                if (File.Exists(initialPath + "/League of Legends/Config/game.cfg"))
+                {
+                    //read
+                    FileStream Rfs = new FileStream(initialPath + "/League of Legends/Config/game.cfg", FileMode.Open);
+                    StreamReader sr = new StreamReader(Rfs);
+                    gameCfg = sr.ReadToEnd();
+                    sr.Close(); Rfs.Close();
+
+
+                    //write
+                    FileStream Wfs = new FileStream("game.cfg", FileMode.OpenOrCreate);
+                    StreamWriter sw = new StreamWriter(Wfs);
+                    sw.Write(gameCfg);
+                    sw.Close(); Wfs.Close();
+                    MessageBox.Show("Safi a zpi Bsahtek hadchi ra khdem mzn");
+                }
+                else
+                {
+                    MessageBox.Show("wa zpi hadchi ra makaynsh");
+                }
+                //</game.cfg>
+
+
+                //------------------------------------------------------------------------------------
+
+
+                //<PersistedSettings.json>
+                string Persisted;
+                if (File.Exists(initialPath + "/League of Legends/Config/PersistedSettings.json"))
+                {
+                    //read
+                    FileStream Rfs1 = new FileStream(initialPath + "/League of Legends/Config/PersistedSettings.json", FileMode.Open);
+                    StreamReader sr1 = new StreamReader(Rfs1);
+                    Persisted = sr1.ReadToEnd();
+                    sr1.Close(); Rfs1.Close();
+
+
+                    //write
+                    FileStream Wfs1 = new FileStream("PersistedSettings.json", FileMode.OpenOrCreate);
+                    StreamWriter sw1 = new StreamWriter(Wfs1);
+                    sw1.Write(Persisted);
+                    sw1.Close(); Wfs1.Close();
+
+                }
+                else
+                {
+                    MessageBox.Show("wa zpi hadchi ra makaynsh");
+                }
+                //</PersistedSettings.json>
+
+
+                //------------------------------------------------------------------------------------
+
+                //<SettingsToPersist.json>
+                string Persist;
+                if (File.Exists(initialPath + "/League of Legends/DATA/CFG/defaults/SettingsToPersist.json"))
+                {
+                    //read
+                    FileStream Rfs2 = new FileStream(initialPath + "/League of Legends/DATA/CFG/defaults/SettingsToPersist.json", FileMode.Open);
+                    StreamReader sr2 = new StreamReader(Rfs2);
+                    Persist = sr2.ReadToEnd();
+                    sr2.Close(); Rfs2.Close();
+
+
+                    //write
+                    FileStream Wfs2 = new FileStream("SettingsToPersist.json", FileMode.OpenOrCreate);
+                    StreamWriter sw2 = new StreamWriter(Wfs2);
+                    sw2.Write(Persist);
+                    sw2.Close(); Wfs2.Close();
+
+                }
+                else
+                {
+                    MessageBox.Show("wa zpi hadchi ra makaynsh");
+                }
+                label7.Text = "You're Importation \n Done Successfully";
+                
+                //</SettingsToPersist.json>
+            }
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            string initialPath = "";
+            bool allow = false;
+
+
+
+            if (Directory.Exists("C:/Riot Games"))
+            {
+                initialPath = "C:/Riot Games";
+                
+                allow = true;
+            }
+            else if (Directory.Exists("E:/Riot Games"))
+            {
+                
+                initialPath = "E:/Riot Games";
+                allow = true;
+            }
+            else if (Directory.Exists("D:/Riot Games"))
+            {
+                
+                initialPath = "D:/Riot Games";
+                allow = true;
+            }
+            else
+            {
+                MessageBox.Show("wa zpi tadir l file f c ola e ola d");
+            }
+
+            if (allow)
+            {
+                //<game.cfg>
+                string gameCfg;
+                if (File.Exists(initialPath + "/League of Legends/Config/game.cfg"))
+                {
+                    //read
+                    FileStream Rfs = new FileStream("game.cfg", FileMode.Open);
+                    StreamReader sr = new StreamReader(Rfs);
+                    gameCfg = sr.ReadToEnd();
+                    sr.Close(); Rfs.Close();
+
+
+                    //write
+                    FileStream Wfs = new FileStream(initialPath + "/League of Legends/Config/game.cfg", FileMode.Open);
+                    StreamWriter sw = new StreamWriter(Wfs);
+                    sw.Write(gameCfg);
+                    sw.Close(); Wfs.Close();
+                    MessageBox.Show("Safi a zpi Bsahtek hadchi ra khdem mzn");
+                }
+                else
+                {
+                    MessageBox.Show("wa zpi hadchi ra makaynch");
+                }
+                //</game.cfg>
+
+                //<SettingsToPersist.json>
+                string Persist;
+                if (File.Exists(initialPath + "/League of Legends/DATA/CFG/defaults/SettingsToPersist.json"))
+                {
+                    //read
+                    FileStream Rfs = new FileStream("SettingsToPersist.json", FileMode.Open);
+                    StreamReader sr = new StreamReader(Rfs);
+                    Persist = sr.ReadToEnd();
+                    sr.Close(); Rfs.Close();
+
+
+                    //write
+                    FileStream Wfs = new FileStream(initialPath + "/League of Legends/DATA/CFG/defaults/SettingsToPersist.json", FileMode.Open);
+                    StreamWriter sw = new StreamWriter(Wfs);
+                    sw.Write(Persist);
+                    sw.Close(); Wfs.Close();
+
+                }
+                else
+                {
+                    MessageBox.Show("wa zpi hadchi ra makaynch");
+                }
+                //</SettingsToPersist.json>
+
+                //<PersistedSettings.json>
+                string Persisted;
+                if (File.Exists(initialPath + "/League of Legends/Config/PersistedSettings.json"))
+                {
+                    //read
+                    FileStream Rfs = new FileStream("PersistedSettings.json", FileMode.Open);
+                    StreamReader sr = new StreamReader(Rfs);
+                    Persisted = sr.ReadToEnd();
+                    sr.Close(); Rfs.Close();
+
+
+                    //write
+                    FileStream Wfs = new FileStream(initialPath + "/League of Legends/Config/PersistedSettings.json", FileMode.Open);
+                    StreamWriter sw = new StreamWriter(Wfs);
+                    sw.Write(Persisted);
+                    sw.Close(); Wfs.Close();
+
+                }
+                else
+                {
+                    MessageBox.Show("wa zpi hadchi ra makaynch");
+                }
+                label7.Text = "You're Exportation \n Done Successfully";
+                //</PersistedSettings.json>
+            }
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            DateTime datetime = DateTime.Now;
+            this.labeltime.Text = datetime.ToString();
+        }
+    }
+}
